4dd86df57430b83c5a417530061add9388085f7a
dovy <dovpeles@gmail.com>
2024-10-02 19:43:03 +0000

    first

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-st.title("🎈 My new app")
+st.title("🎈 Jojo app")
 st.write(
     "Let's start building! For jojo and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
