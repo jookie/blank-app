ed66f623cebc5286b07fc2222027bc26a9f2ffe1
dovy <dovpeles@gmail.com>
2024-10-02 18:45:10 +0000

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
+
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building! For jojo and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
