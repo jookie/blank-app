a733107630bd89bb4d0370033418ef1d9d40d759
dovy <dovpeles@gmail.com>
2024-10-06 17:05:32 -0400

    Update streamlit_app.py
    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+reimport streamlit as st
 st.title("🎈 Jojo app from dov INSIDE GITHUB")
 st.write(
     "Let's start building! For jojo and inspiration, Jojo app from dov INSIDE GITHUB head over to [docs.streamlit.io](https://docs.streamlit.io/)
